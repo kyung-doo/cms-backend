0a3c1b83c178835d4394dad0abc5316c03175722
kyung-doo <hohomkd@naver.com>
2018-12-24 01:13:55 +0900

    ff

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ db.once('open',function(){
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
 
-//app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
